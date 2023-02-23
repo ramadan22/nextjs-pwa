19e1b6b76a0e3bcb86420f997385ff70ca7cf5b0
harisramadan <harisramadan22@gmail.com>
2023-02-24 00:16:30 +0700

    add pwa

---
+++ pages/page2.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+export default function page2() {
+  return (
+    <div className="p-0">
+      <Navbar />
+      <h1 style={{ marginTop: '100px' }}>ini page 2</h1>
+    </div>
+  );
+}
